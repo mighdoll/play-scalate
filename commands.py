74f3a62f4931e001ee1bf5ece02ad57c267863d4
peter hausel <peter_kovac11@hotmail.com>
2010-04-11 18:31:16 -0400

    fix path issue

--- commands.py
+++ commands.py
@@ -1,7 +1,7 @@
 # Scala
 import sys,os,inspect
 
-from play.utils import *
+from framework.pym.utils import *
 
 MODULE = 'scalate'
 
@@ -22,17 +22,17 @@ def after(**kargs):
     env = kargs.get("env")
     if command == 'new':
         module_dir = inspect.getfile(inspect.currentframe()).replace("commands.py","")
-        shutil.copyfile(os.path.join(module_dir,'resources/Application.scala'), os.path.join(application_path, 'app/controllers/Application.scala'))
-        shutil.copyfile(os.path.join(module_dir,'resources/Model.scala'), os.path.join(application_path, 'app/models/Model.scala'))
-        shutil.copyfile(os.path.join(module_dir,'resources/index.ssp'), os.path.join(application_path, 'app/views/Application/index.ssp'))
-        shutil.copyfile(os.path.join(module_dir,'resources/default.ssp'), os.path.join(application_path, 'app/views/default.ssp'))
-        shutil.copyfile(os.path.join(module_dir,'resources/500.scaml'), os.path.join(application_path, 'app/views/errors/500.scaml'))
-        shutil.copyfile(os.path.join(module_dir,'resources/500.css'), os.path.join(application_path, 'public/stylesheets/500.css'))
-        f = open(os.path.join(application_path, 'conf/application.conf'),'a')
+        shutil.copyfile(os.path.join(module_dir,'resources/Application.scala'), os.path.join(app.path, 'app/controllers/Application.scala'))
+        shutil.copyfile(os.path.join(module_dir,'resources/Model.scala'), os.path.join(app.path, 'app/models/Model.scala'))
+        shutil.copyfile(os.path.join(module_dir,'resources/index.ssp'), os.path.join(app.path, 'app/views/Application/index.ssp'))
+        shutil.copyfile(os.path.join(module_dir,'resources/default.ssp'), os.path.join(app.path, 'app/views/default.ssp'))
+        shutil.copyfile(os.path.join(module_dir,'resources/500.scaml'), os.path.join(app.path, 'app/views/errors/500.scaml'))
+        shutil.copyfile(os.path.join(module_dir,'resources/500.css'), os.path.join(app.path, 'public/stylesheets/500.css'))
+        f = open(os.path.join(app.path, 'conf/application.conf'),'a')
         f.write('\n\n#scalate config\nscalate=ssp\njvm.memory=-Xmx256M -Xms32M')
         f.close()
-        os.remove(os.path.join(application_path, 'app/views/Application/index.html'))
-        os.remove(os.path.join(application_path, 'app/views/main.html'))
+        os.remove(os.path.join(app.path, 'app/views/Application/index.html'))
+        os.remove(os.path.join(app.path, 'app/views/main.html'))
 
 
 def before(**kargs):
@@ -44,9 +44,9 @@ def before(**kargs):
         app.check()
         java_cmd = app.java_cmd(args)
         #add precompiled classes to classpath
-        app.cp_args += ":"+os.path.normpath(os.path.join(application_path,'tmp/classes'))
-        if os.path.exists(os.path.join(application_path, 'tmp')):
-            shutil.rmtree(os.path.join(application_path, 'tmp'))
+        app.cp_args += ":"+os.path.normpath(os.path.join(app.path,'tmp/classes'))
+        if os.path.exists(os.path.join(app.path, 'tmp')):
+            shutil.rmtree(os.path.join(app.path, 'tmp'))
         # replace last element with the console app
         java_cmd[len(java_cmd)-1]="play.mvc.PreCompiler"
         java_cmd.insert(2, '-Xmx256M -Xms32M')
