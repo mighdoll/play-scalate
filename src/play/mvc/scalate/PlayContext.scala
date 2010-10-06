6516c319746328b087d069290d33e81187edc58b
Atsuhiko Yamanaka <atsuhiko.yamanaka@gmail.com>
2010-10-07 01:06:01 +0900

    supporting url("action", List(param1,param2,...)) syntax.

--- src/play/mvc/scalate/PlayContext.scala
+++ src/play/mvc/scalate/PlayContext.scala
@@ -31,5 +31,55 @@ object PlayContext {
        case ex: ClassCastException => throw new ClassCastException("url tag takes a Map[String,AnyRef] as a second parameter")
      }
    }
+
+   def url(uri:String, params:List[Any]):String = {
+
+    def isSimpleParam[X](typ: Class[X]): Boolean = {
+      classOf[Number].isAssignableFrom(typ) || 
+      typ.equals(classOf[String]) ||
+      typ.isPrimitive
+    }
+
+     import play.mvc.ActionInvoker
+     import java.lang.reflect.Method
+     import play.classloading.enhancers.LocalvariablesNamesEnhancer.LocalVariablesNamesTracer
+     import play.exceptions.NoRouteFoundException
+     import play.mvc.Router
+     import play.data.binding.Unbinder
+
+     val action = uri
+     try{
+       val r = new java.util.HashMap[String, java.lang.Object]
+       val actionMethod = ActionInvoker.getActionMethod(action)(1).asInstanceOf[Method]
+       val names = actionMethod.getDeclaringClass.getDeclaredField("$" + actionMethod.getName + LocalVariablesNamesTracer.computeMethodHash(actionMethod.getParameterTypes)).get(null).asInstanceOf[Array[String]]
+
+       if(names.length < params.length){
+         throw new NoRouteFoundException(action, null);
+       }
+
+       for{
+         (param, i) <- params.zipWithIndex
+         val name = if(i < names.length) names(i) else ""}{
+         if(param.isInstanceOf[Router.ActionDefinition] && param != null){
+           Unbinder.unBind(r, param.toString, name)
+         } 
+         else if (isSimpleParam(actionMethod.getParameterTypes.apply(i))){
+           if (param != null) {
+             Unbinder.unBind(r, param.toString, name)
+           }
+         } 
+         else {
+           Unbinder.unBind(r, param, name)
+         }
+       }
+
+       Router.reverse(action, r).url
+     } 
+     catch {
+       case ex: ClassCastException => 
+         throw new ClassCastException("url tag takes a List[String,AnyRef] as a second parameter")
+     }
+   }
+
 }
 
